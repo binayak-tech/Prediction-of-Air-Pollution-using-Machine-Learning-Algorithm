e5bc17797929d2b51356d5bd8bcc48d725b6a923
Binayak Purohit <binayakpurohit007@gmail.com>
2023-04-30 05:59:57 +0530

    minor changes in settings.py

--- air_pollution/air_pollution/settings.py
+++ air_pollution/air_pollution/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v$pr5@17tbw1j!p^ifhqm!4zd2f5%a8*54bmy8=o&cu6#=m7x7'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
